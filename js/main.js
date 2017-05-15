03fc2c57dfa857b13d4404731523ceac335daf3d
“espelucero” <espe.lucero@gmail.com>
2017-05-15 14:01:42 -0400

    Validando nombre en formulario

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 function validateForm(){
-	/* Escribe tú código aquí */
-}
+	var nombre = document.getElementById("name").value;
+	var aoellido = document.getElementById("lastname").value;
+	var email = document.getElementById("input-email").value;
+	var password = document.getElementById("input-password").value;
+	var tiposBici = document.getElementsByClassName("form-control").value;
+
+
+	if (nombre == "") {
+		
+		var nombreVacio = document.getElementsByClassName('name-container input-box')[0];
+		var span = document.createElement('span');
+		var requerido1 = document.createTextNode("Debe ingresar su nombre");
+		nombreVacio.appendChild(span);
+		span.appendChild(requerido1);
+
+
+	}
+
+	}
